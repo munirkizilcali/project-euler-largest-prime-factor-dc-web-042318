abe630de06c5940b85261eb5659f77783b4d07f5
munirkizilcali <munir.kizilcali@gmail.com>
2018-09-10 09:54:42 -0400

    high complexity solution

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,17 @@
+require 'prime'
+
+
 # Enter your procedural solution here!
+def largest_prime_factor(number)
+	factors = []
+
+	(1...number).each do |i| 
+		if number % i == 0 && Prime.prime?(i)
+			factors.push(i)
+		end
+	end
+	factors.last
+end
+
+
+
